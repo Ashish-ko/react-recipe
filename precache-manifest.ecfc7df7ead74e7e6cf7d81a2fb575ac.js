7d0114286077202028d1407a2f0af3d984227f30
Ashish <ashish1311998@gmail.com>
2022-05-11 00:18:22 +0530

    Updates

---
+++ precache-manifest.ecfc7df7ead74e7e6cf7d81a2fb575ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b1e3705a7e5fcda181f6baccff8a1e",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "e68ee7c60c4fe4525480",
+    "url": "/react-recipe/static/css/main.ddf21bb3.chunk.css"
+  },
+  {
+    "revision": "188037c5abeaf672ef48",
+    "url": "/react-recipe/static/js/2.48b3b32d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-recipe/static/js/2.48b3b32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68ee7c60c4fe4525480",
+    "url": "/react-recipe/static/js/main.6d534131.chunk.js"
+  },
+  {
+    "revision": "63753938abeb1cd4e2a6",
+    "url": "/react-recipe/static/js/runtime-main.3079b670.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Bfood-recipe%7D/precache-manifest.dbbdb7e636c44ace390ca70184ca0b00.js"
+  "/react-recipe/precache-manifest.ecfc7df7ead74e7e6cf7d81a2fb575ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Bfood-recipe%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-recipe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
